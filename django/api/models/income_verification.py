ddad73fc28dd4afb7fcde34da1e8a3642a3f0842
Jamie Popkin <popkinj@littleearth.ca>
2022-02-07 15:13:43 -0800

    Installing scheduler

--- django/api/models/income_verification.py
+++ django/api/models/income_verification.py
@@ -1,85 +1,34 @@
-<<<<<<< HEAD
-from django.db import models
-
-class IncomeVerification():
-
-    create_timestamp = models.DateTimeField(
-        auto_now_add=True,
-        blank=True,
-        null=True
-    )
-
-    update_timestamp = models.DateTimeField(
-        auto_now=True,
-        blank=True,
-        null=True
-    )   
-
-    sin = models.CharField(
-=======
 from django.db.models import CharField, IntegerField
 from auditable.models import Auditable
 
 
 class IncomeVerification(Auditable):
     sin = CharField(
->>>>>>> upstream/release-0.1.0
         blank=True,
         null=True,
         max_length=250,
         unique=False
     )
-<<<<<<< HEAD
-
-    surname = models.CharField(
-=======
     last_name = CharField(
->>>>>>> upstream/release-0.1.0
         blank=True,
         null=True,
         max_length=250,
         unique=False
     )
-<<<<<<< HEAD
-
-    first_name = models.CharField(
-=======
     first_name = CharField(
->>>>>>> upstream/release-0.1.0
         blank=True,
         null=True,
         max_length=250,
         unique=False
     )
-<<<<<<< HEAD
-
-    date_of_birth = models.IntegerField(
-        blank=True,
-        null=True
-    )
-
-    tax_year = models.IntegerField(
-=======
     date_of_birth = IntegerField(
         blank=True,
         null=True
     )
     tax_year = IntegerField(
->>>>>>> upstream/release-0.1.0
         blank=True,
         null=True
     )
 
     class Meta:
-<<<<<<< HEAD
-        db_table = "income_verification"
-
-
-# SIN (Social Insurance Number) - encrypted (Richard to provide info/link)
-# Surname (30 character limit)
-# First name (30 character limit)
-# Date of birth (yyyymmdd) (numeric 8)
-# Tax year (numeric 4)
-=======
         db_table = 'income_verification'
->>>>>>> upstream/release-0.1.0
--- django/api/settings.py
+++ django/api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'corsheaders',
     'django_filters',
+    'django-extensions'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
