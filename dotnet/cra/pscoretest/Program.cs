4d49298ecdadd275d39d4ce67a4fe7177e08af53
Kuan Fan <kuan.fan@gov.bc.ca>
2022-03-07 15:04:18 -0800

    add dotnet

---
+++ dotnet/cra/pscoretest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace pscoretest // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                var results = ps.AddScript("Get-Command").Invoke();
+
+
+                foreach(var item in results)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
