6fba8a86bcdcd0a1cadefd81807592a89f0d1e5a
emi-hi <emily.hillier@gmail.com>
2022-03-09 15:58:18 -0800

    -adds logic for questions/enabling login -adds graphic at bottom of page -moves bceid login into eligibility page -some styling changes -linting

--- frontend/src/eligibility_page/components/EligibilityPage.js
+++ frontend/src/eligibility_page/components/EligibilityPage.js
@@ -2,90 +2,49 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import React from 'react';
 import EligibilityQuestions from './EligibilityQuestions';
+import RebateTable from '../RebateTable';
+import { useKeycloak } from '@react-keycloak/web';
 
 const EligibilityPage = (props) => {
-  const {
-    handleInputChange, handleSubmit, year, questions, setQuestions, handleCheckboxChange,
-  } = props;
-
+  const { year, questions, setQuestions, handleCheckboxChange, eligible } =
+    props;
+  const { keycloak } = useKeycloak();
   return (
     <div>
       <div>
-
         <h1>Electric Vehicle Rebates</h1>
         <h2>for Individuals and Households</h2>
       </div>
       <div>
         <p>
-          Rebates of up to $4,000 are available from the B.C. Government towards the purchase of a
-          new electric vehicle. Your individual or household income determines the rebate amount.
+          Rebates of up to $4,000 are available from the B.C. Government towards
+          the purchase of a new electric vehicle. Your individual or household
+          income determines the rebate amount.
         </p>
         <h3>How it works</h3>
         <div>
           <ol>
-            <li>Complete the following eligibility questions and online application process.</li>
             <li>
-              If you are eligible, a rebate code will be emailed to you that indicates
-              your rebate amount.
-
+              Complete the following eligibility questions and online
+              application process.
             </li>
             <li>
-              The rebate code is associated with your B.C. Driver&apos;s Licence number.
-              They are both needed at the car dealership to receive the rebate discount at the
-              time of vehicle purchase.
+              If you are eligible, a rebate code will be emailed to you that
+              indicates your rebate amount.
+            </li>
+            <li>
+              The rebate code is associated with your B.C. Driver&apos;s Licence
+              number. They are both needed at the car dealership to receive the
+              rebate discount at the time of vehicle purchase.
             </li>
           </ol>
         </div>
-        <h4>Rebate Application Type (Individual or Household)</h4>
-        <p>
-          If your total individual income was $100,000 or less or your total household income was
-          $165,000 or less you are eligible for a rebate at the following rates:
-        </p>
-        <table>
-          <tbody>
-            <tr>
-              <th>Individual Income</th>
-              <th>Household Income</th>
-              <th>
-                Rebate Amount Range
-                <sup>1</sup>
-              </th>
-            </tr>
-            <tr>
-              <td>Less than $80,000</td>
-              <td>Less than $125,000</td>
-              <td>$2,000 - $4,000</td>
-            </tr>
-            <tr>
-              <td>$80,001 - 90,000</td>
-              <td>$125,001 - 145,000</td>
-              <td>$1,000 - $2,000</td>
-            </tr>
-            <tr>
-              <td>$90,001 - 100,000</td>
-              <td>$145,001 - 165,000</td>
-              <td>$500 - $1,000</td>
-            </tr>
-          </tbody>
-        </table>
-        <sup>1</sup>
-        <sub>
-          BEV (Battery Electric Vehicle) and long-range PHEV (Plug-in Hybrid Electric Vehicle)
-          receive the higher rebate amount.
-        </sub>
-        <p>
-          Your income will be verified with the Canada Revenue Agency based on your
-          {' '}
-          {year}
-          {' '}
-          notice of assessment (line 15000).
-        </p>
-        <h3>
-          Determine your eligibility for a rebate
-        </h3>
+        <RebateTable year={year} />
+
+        <h3>Determine your eligibility for a rebate</h3>
         {questions.map((question, index) => (
           <EligibilityQuestions
-            question={question}
+            question={question.question}
             index={index}
             setQuestions={setQuestions}
             handleCheckboxChange={handleCheckboxChange}
@@ -95,19 +54,47 @@ const EligibilityPage = (props) => {
           <h3>What you will need to complete this application</h3>
           <ul>
             <li>
-              Your Driver's Licence number to be associated with the rebate code.
+              Your Driver's Licence number to be associated with the rebate
+              code.
             </li>
             <li>
-              A Basic BCeID, an image of your B.C. Driver's Licence and a secondary piece of
-              ID to upload.
+              A Basic BCeID, an image of your B.C. Driver's Licence and a
+              secondary piece of ID to upload.
             </li>
             <li>
-              Your Social Insurance Number and CRA income disclosure consent to confirm your income.
+              Your Social Insurance Number and CRA income disclosure consent to
+              confirm your income.
             </li>
           </ul>
-          For a household application your spouse or common law partner will also need to
-          confirm their identity and provide CRA income disclosure consent, they do not
-          require a driver's licence.
+          For a household application your spouse or common law partner will
+          also need to confirm their identity and provide CRA income disclosure
+          consent, they do not require a driver's licence.
+        </div>
+      </div>
+      <div id="start-application">
+        <h2>Start your rebate application</h2>
+        <div id="bceid-login">
+          <h2>BCeID</h2>
+          <button
+            type="button"
+            className="button"
+            disabled={!eligible}
+            onClick={() =>
+              keycloak.login({
+                idpHint: 'bceid-basic',
+                redirectUri: `${window.location.origin}/form`
+              })
+            }
+          >
+            Login with BCeID
+          </button>
+          <div>
+            <a href="">Get a Basic BCeID account</a>
+            <p>
+              The name associated with your BCeID account must match your legal
+              first and last name on your identification documents
+            </p>
+          </div>
         </div>
       </div>
     </div>
