2835e6524ad69e15541559959bce04ba844bacbb
emi-hi <emily.hillier@gmail.com>
2022-03-21 14:34:07 -0700

    -removes comments

--- frontend/src/stories/Consent.stories.jsx
+++ frontend/src/stories/Consent.stories.jsx
@@ -6,12 +6,10 @@ export default {
   title: 'Consent',
   component: ApplicationConsent,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen'
   }
 };
 
 const Template = (args) => <ApplicationConsent {...args} />;
 
-// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const Consent = Template.bind({});
