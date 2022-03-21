265598e3ae2b0125efda690bcc96d7673eb6209f
Naomi Aro <naomi.aro@gov.bc.ca>
2022-03-21 11:28:55 -0700

    initial form fields logging to console

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
+import FormGroup from '@mui/material/FormGroup';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
 
-export const Form = () => {
+const Form = () => {
   const { control, handleSubmit } = useForm({
     defaultValues: {
-      sin: null,
+      sin: '',
       first_name: '',
       last_name: '',
       dob: '',
@@ -19,51 +21,69 @@ export const Form = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label>SIN</label>
-      <Controller
-        name="sin"
-        control={control}
-        render={({ field }) => (
-          <TextField type="number" inputProps={{ maxLength: 9 }} {...field} />
-        )}
-      />
-      <label>Surname</label>
-      <Controller
-        name="last_name"
-        control={control}
-        render={({ field }) => (
-          <TextField inputProps={{ maxLength: 30 }} {...field} />
-        )}
-      />
-      <label>First Name</label>
-      <Controller
-        name="first_name"
-        control={control}
-        render={({ field }) => (
-          <TextField inputProps={{ maxLength: 30 }} {...field} />
-        )}
-      />
-      <label>Date of Birth</label>
-      <Controller
-        name="dob"
-        control={control}
-        render={({ field }) => (
-          <TextField
-            helperText="yyyymmdd"
-            inputProps={{ maxLength: 4 }}
-            {...field}
-          />
-        )}
-      />
-      <label>Tax Year</label>
-      <Controller
-        name="tax_year"
-        control={control}
-        render={({ field }) => (
-          <TextField inputProps={{ maxLength: 4 }} {...field} />
-        )}
-      />
-      <Button variant="contained">Submit</Button>
+      <FormGroup>
+        <InputLabel htmlFor="sin">SIN</InputLabel>
+        <Controller
+          name="sin"
+          control={control}
+          render={({ field }) => (
+            <TextField id="sin" inputProps={{ maxLength: 9 }} {...field} />
+          )}
+        />
+      </FormGroup>
+      <FormGroup>
+        <InputLabel htmlFor="last_name">Surname</InputLabel>
+        <Controller
+          name="last_name"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              id="last_name"
+              inputProps={{ maxLength: 30 }}
+              {...field}
+            />
+          )}
+        />
+      </FormGroup>
+      <FormGroup>
+        <InputLabel htmlFor="first_name">First Name</InputLabel>
+        <Controller
+          name="first_name"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              id="first_name"
+              inputProps={{ maxLength: 30 }}
+              {...field}
+            />
+          )}
+        />
+      </FormGroup>
+      <FormGroup>
+        <InputLabel htmlFor="dob">Date of Birth</InputLabel>
+        <Controller
+          name="dob"
+          control={control}
+          render={({ field }) => (
+            <TextField id="dob" inputProps={{ maxLength: 4 }} {...field} />
+          )}
+        />
+      </FormGroup>
+      <FormGroup>
+        <InputLabel htmlFor="tax_year">Tax Year</InputLabel>
+        <Controller
+          name="tax_year"
+          control={control}
+          render={({ field }) => (
+            <TextField id="tax_year" inputProps={{ maxLength: 4 }} {...field} />
+          )}
+        />
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </FormGroup>
     </form>
   );
 };
+
+export default Form;
