22643b7b4026ebe14920500d16f9411ac8784121
Naomi Aro <naomi.aro@gov.bc.ca>
2022-03-22 18:19:15 -0700

    simpler admin page

--- django/api/admin.py
+++ django/api/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models.income_verification import IncomeVerification
+from django.contrib.admin.templatetags import admin_modify
+submit_row = admin_modify.submit_row
+
+
+def submit_row_custom(context):
+    ctx = submit_row(context)
+    ctx['show_save_and_add_another'] = False
+    ctx['show_save_and_continue'] = False
+    return ctx
+
+
+admin_modify.submit_row = submit_row_custom
+
 
 @admin.register(IncomeVerification)
 class IncomeVerificationAdmin(admin.ModelAdmin):
