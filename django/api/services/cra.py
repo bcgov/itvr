50d8c7936a09c5ba64d1f64aeba2c57de921f41a
Jamie Popkin <popkinj@littleearth.ca>
2022-04-04 10:34:13 -0700

    CRA File Generation, Reading and Testing (#50)
    * All back end requirements working with latest versions
    
    * Removing all sample code and data
    
    * Restored order_by.py
    
    * New model file
    
    * Here is the model
    
    * Grabbing list of files on ftp server
    
    * Not sure how that line got in there
    
    * Isolating the file name
    
    * Uploading and downloading files
    
    * Installing scheduler
    
    * Testing out this job scheduler
    
    * Here's cron
    
    * Here's cron
    
    * Running cron
    
    * log cile
    
    * Closing file
    
    * Newline
    
    * Cron still not running
    
    * Cron is running
    
    * Ignore the log file
    
    * Removing unnecessary dependency
    
    * Newline
    
    * Here is the module for reading a CRA return file.
    
    * Another duplication removed
    
    * Complete modularize... is that a word 😆... of the CRA function and test
    
    * Moving this line up here
    
    * Starting to make this model
    
    * cra write module
    
    * All the body entries
    
    * Here's a test
    
    * All tests for CRA files

--- django/api/services/cra.py
+++ django/api/services/cra.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 ##
 # Read a text file that has been posted by CRA
 # INPUT: A string representing a text file
@@ -15,3 +17,72 @@ def read(file):
     income = line[21:30].lstrip("0")
     results.append({'sin':sin,'year':year,'income':income}) # Add to array
   return results # Return results
+
+##
+# Write a CRA request file
+# INPUT: A dictionary of values to write to the file
+# OUTPUT: A string representing a text file
+#
+def write(data):
+  file = "" # String to return
+
+  today = date.today().strftime("%Y%m%d") # Get today's date
+
+  ####################### Write the header ##############################
+  file += '7100' # Request transaction code
+  file += ' ' * 24 # Blank space
+
+  file += today # 
+  file += ' ' # Blank space
+
+  file += 'BCGSP00521' # Requesting institution code TODO: make this dynamic
+
+  file += ' ' * 99 # Blank space
+
+  file += '0\n' # Blank space
+
+
+  ####################### Write the body ##############################
+  for row in data:
+    file += '7101' # Request transaction code
+    file += row['sin'] # SIN
+    file += ' ' * 4 # Blank space
+    file += '0020' # Sub-code
+    file += row['family_name'] # Family name
+
+    file += ' ' * (30 - len(row['family_name'])) # Blank space
+    file += row['given_name'] # Given name
+
+    file += ' ' * (30 - len(row['given_name'])) # Blank space
+    file += row['birth_date'].replace('-','') # Birth date
+
+    file += row['year'] # Year
+    file += ' ' * 14 # Blank space
+
+    file += 'BCGS' # Program area code
+    file += '1234' # Record identification number (optional)
+
+    file += ' ' * 31 # Blank space
+    file += '0\n' # Delimiter
+
+
+  ####################### Write the trailer ###########################
+  file += '7102' # Request transaction code 
+  file += ' ' * 24 # Blank space
+
+  file += today # Request date
+  file += ' ' # Blank space
+
+  file += 'BCGSP00521' # Requesting institution code TODO: make this dynamic
+
+  file += ' ' * 6 # Blank space
+
+  file += '0' * 8 # Number of records in file TODO: make this dynamic
+
+  file += ' ' * 85 # Blank space
+
+  file += '0\n' # terminating character 
+
+  
+  ####################### Return the file ##############################
+  return file
