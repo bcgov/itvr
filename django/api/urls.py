cdeb1f0cc7b45f3bb2f4997005a932d9efcf10ce
Naomi Aro <naomi.aro@gov.bc.ca>
2022-04-04 14:18:49 -0700

    figure out routing urls

--- django/api/urls.py
+++ django/api/urls.py
@@ -4,8 +4,15 @@ from rest_framework import routers
 
 from api.viewsets.application_form import ApplicationFormViewset
 
-ROUTER = routers.DefaultRouter(trailing_slash=False)
-ROUTER.register(r'application-form/?', ApplicationFormViewset)
+
+class OptionalSlashRouter(routers.DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.trailing_slash = '/?'
+
+
+ROUTER = OptionalSlashRouter()
+ROUTER.register(r'application-form', ApplicationFormViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
