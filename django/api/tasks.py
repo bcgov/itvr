70e92e9c028b492090bf4d81f980beecd2fbba2b
Naomi Aro <naomi.aro@gov.bc.ca>
2022-05-02 16:53:44 -0700

    Test sending emails

--- django/api/tasks.py
+++ django/api/tasks.py
@@ -10,34 +10,27 @@ from requests.auth import HTTPBasicAuth
 LOGGER = logging.getLogger(__name__)
 
 
-def get_email_service_token() -> {}:
+def get_email_service_token() -> str:
     client_id = settings.EMAIL["EMAIL_SERVICE_CLIENT_ID"]
     client_secret = settings.EMAIL["EMAIL_SERVICE_CLIENT_SECRET"]
     url = settings.EMAIL["CHES_AUTH_URL"]
     payload = {"grant_type": "client_credentials"}
     header = {"content-type": "application/x-www-form-urlencoded"}
 
-    try:
-        token_rs = requests.post(
-            url,
-            data=payload,
-            auth=HTTPBasicAuth(client_id, client_secret),
-            headers=header,
-            verify=True,
-        )
-        if not token_rs.status_code == 200:
-            LOGGER.error("Error: Unexpected response", token_rs.text.encode("utf8"))
-            return
-        json_obj = token_rs.json()
-        return json_obj
-    except requests.exceptions.RequestException as e:
-        LOGGER.error("Error: {}".format(e))
-        return
+    token_rs = requests.post(
+        url,
+        data=payload,
+        auth=HTTPBasicAuth(client_id, client_secret),
+        headers=header,
+        verify=True,
+    )
+    token_rs.raise_for_status()
+    return token_rs.json()["access_token"]
 
 
 def send_email(
     recipient_email: str, application_id: str, message: str, cc_list: list
-) -> {}:
+) -> None:
     sender_email = settings.EMAIL["SENDER_EMAIL"]
     sender_name = settings.EMAIL["SENDER_NAME"]
     url = settings.EMAIL["CHES_EMAIL_URL"]
@@ -45,12 +38,7 @@ def send_email(
     subject = "CleanBC Go Electric - Application #{}".format(application_id)
     bodyType = "html"
 
-    token = get_email_service_token()
-    if not token or "access_token" not in token:
-        LOGGER.error("No email service token provided", token)
-        return
-    auth_token = token["access_token"]
-
+    auth_token = get_email_service_token()
     sender_info = formataddr((str(Header(sender_name, "utf-8")), sender_email))
 
     data = {
@@ -70,22 +58,12 @@ def send_email(
         "Authorization": "Bearer " + auth_token,
         "Content-Type": "application/json",
     }
-    try:
-        response = requests.post(url, data=json.dumps(data), headers=headers)
-        if not response.status_code == 201:
-            LOGGER.error("Error: Email failed! %s", response.text.encode("utf8"))
-            return
 
-        email_res = response.json()
-        if email_res:
-            LOGGER.debug("Email sent successfully!", email_res["messages"][0]["msgId"])
-            return
-    except requests.exceptions.RequestException as e:
-        LOGGER.error("Error: {}".format(e))
-        return
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    response.raise_for_status()
 
 
-def send_individual_confirm(recipient, id):
+def send_individual_confirm(recipient_email, application_id):
     message = """
         We have received your application for a rebate under the CleanBC Go Electric Passenger Vehicle Rebate program.
 
@@ -95,10 +73,11 @@ def send_individual_confirm(recipient, id):
 
         Please feel free to contact us at ZEVPrograms@gov.bc.ca
         """
-    send_email(recipient, id, message, cc_list=[])
+    send_email(recipient_email, application_id, message, cc_list=[])
 
 
-def send_spouse_initial_message(recipient, id, initiator_email):
+def send_spouse_initial_message(recipient_email, application_id, initiator_email):
+    origin = settings.CORS_ORIGIN_WHITELIST
     message = """
         Dear Applicant,
 
@@ -106,18 +85,18 @@ def send_spouse_initial_message(recipient, id, initiator_email):
 
         To finish the rebate application please click on the following link:
 
-        http://localhost:3000/household?q={}
+        {origin}/household?q={application_id}
 
         Questions?
 
         Please feel free to contact us at ZEVPrograms@gov.bc.ca
         """.format(
-        id
+        origin, application_id
     )
-    send_email(recipient, id, message, cc_list=[initiator_email])
+    send_email(recipient_email, application_id, message, [initiator_email])
 
 
-def send_household_confirm(recipient, id, initiator_email):
+def send_household_confirm(recipient_email, application_id, initiator_email):
     message = """
         Thank you.
 
@@ -129,4 +108,4 @@ def send_household_confirm(recipient, id, initiator_email):
 
         Please feel free to contact us at ZEVPrograms@gov.bc.ca
         """
-    send_email(recipient, id, message, [initiator_email])
+    send_email(recipient_email, application_id, message, [initiator_email])
