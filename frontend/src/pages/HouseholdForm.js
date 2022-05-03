6326f0397586ceaf2da45e35df355e447efefe2a
tim738745 <98717409+tim738745@users.noreply.github.com>
2022-05-03 14:15:35 -0700

    itvr-284--household-application-frontend-validation (#94)

--- frontend/src/pages/HouseholdForm.js
+++ frontend/src/pages/HouseholdForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import jwt_decode from 'jwt-decode';
 import SpouseForm from '../components/SpouseForm';
 import { useKeycloak } from '@react-keycloak/web';
@@ -12,11 +12,30 @@ const HouseholdFormPage = () => {
   // we can validate the token server side
   const decoded = jwt_decode(keycloak.token);
 
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const [errorsExistCounter, setErrorsExistCounter] = useState(0);
+  const errorMessageRef = useRef(null);
+
+  useEffect(() => {
+    if (numberOfErrors > 0) {
+      errorMessageRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [errorsExistCounter]);
+
   return (
     <div>
       Hello BCeID {decoded.preferred_username}
       <Layout>
-        <SpouseForm id={householdApplicationId} />
+        {numberOfErrors > 0 && (
+          <span className="error" ref={errorMessageRef}>
+            Errors below, please ensure all fields are complete
+          </span>
+        )}
+        <SpouseForm
+          id={householdApplicationId}
+          setNumberOfErrors={setNumberOfErrors}
+          setErrorsExistCounter={setErrorsExistCounter}
+        />
       </Layout>
     </div>
   );
