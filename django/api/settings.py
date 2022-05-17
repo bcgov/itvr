47b135313b4a0239300d4c4ecff72031eadee1ef
tim738745 <tim.chen@gov.bc.ca>
2022-05-17 11:52:04 -0700

    itvr--timezone-fix

--- django/api/settings.py
+++ django/api/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = "Canada/Pacific"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
