a0d87f9f87e12a8602526df43bfe46816f1961b6
Naomi Aro <naomi.aro@gov.bc.ca>
2022-05-25 15:41:45 -0700

    remove code smell :D

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,3 @@
-// window.itvr_config = {
-//   "REACT_APP_KEYCLOAK_CLIENT_ID": "itvr-2674",
-//   "REACT_APP_KEYCLOAK_REALM": "onestopauth-basic",
-//   "REACT_APP_KEYCLOAK_URL": "https://dev.oidc.gov.bc.ca/auth/",
-//   "REACT_APP_API_BASE": "https://itvr-backend-dev-102.apps.silver.devops.gov.bc.ca"
-// };
-
 export const KEYCLOAK_CLIENT_ID = window.itvr_config
   ? window.itvr_config.REACT_APP_KEYCLOAK_CLIENT_ID
   : process.env.REACT_APP_KEYCLOAK_CLIENT_ID;
