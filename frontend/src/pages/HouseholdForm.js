8bf92eaf4de17cff5377e86d5a05a1917c82b4e0
vibhishan <vibhsian.vel@quartech.com>
2022-06-01 22:06:32 +0530

    add metadata and favicon

--- frontend/src/pages/HouseholdForm.js
+++ frontend/src/pages/HouseholdForm.js
@@ -3,6 +3,7 @@ import SpouseForm from '../components/SpouseForm';
 import Layout from '../components/Layout';
 import { useKeycloak } from '@react-keycloak/web';
 import { useSearchParams } from 'react-router-dom';
+import {Helmet} from "react-helmet";
 
 const HouseholdFormPage = () => {
   const [searchParams] = useSearchParams();
@@ -21,6 +22,9 @@ const HouseholdFormPage = () => {
 
   return (
     <div>
+      <Helmet>
+        <title>Passenger Vehicle Rebate Application Form – CleanBC Go Electric</title>
+      </Helmet>
       <Layout>
         {numberOfErrors > 0 && (
           <span className="error" ref={errorMessageRef}>
