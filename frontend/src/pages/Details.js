6b83e1b64b512031e74e7383793757577663d66a
vibhiquartech <105294710+vibhiquartech@users.noreply.github.com>
2022-06-01 23:46:58 +0530

    add metadata and favicon (#141)
    Co-authored-by: vibhishan <vibhsian.vel@quartech.com>

--- frontend/src/pages/Details.js
+++ frontend/src/pages/Details.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Layout from '../components/Layout';
 import ApplicationSummary from '../components/ApplicationSummary';
 import { useParams } from 'react-router-dom';
+import {Helmet} from "react-helmet";
 
 const ApplicationSummaryPage = () => {
   const { id } = useParams();
   return (
     <div>
+      <Helmet>
+        <title>Passenger Vehicle Rebate Application Confirmation  – CleanBC Go Electric</title>
+      </Helmet>
       <Layout>
         <ApplicationSummary id={id} />
       </Layout>
