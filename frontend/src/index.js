7acdba56b364a7af47091fb18a305cc5f92c3f19
tim738745 <tim.chen@gov.bc.ca>
2022-06-08 11:56:32 -0700

    itvr328--add-footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { getKeycloak, keycloakInitOptions } from './keycloak';
 import AppRouter from './routes';
+import Footer from './components/Footer';
 
 import './styles/index.scss';
 import Loading from './components/Loading';
@@ -19,6 +20,7 @@ ReactDOM.render(
   >
     <QueryClientProvider client={queryClient}>
       <AppRouter />
+      <Footer />
     </QueryClientProvider>
   </ReactKeycloakProvider>,
   document.getElementById('root')
