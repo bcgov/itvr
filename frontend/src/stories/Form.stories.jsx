f776c73aa5e5441fab34f11ed69d0a86493a91bc
emi-hi <emily.hillier@gmail.com>
2022-06-09 09:53:29 -0700

    -adds story for spouse form

--- frontend/src/stories/Form.stories.jsx
+++ frontend/src/stories/Form.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from '../components/Form';
+import SpouseForm from '../components/SpouseForm';
 
 export default {
   title: 'ITVR/RebateForm',
@@ -10,5 +11,9 @@ export default {
 };
 
 const Template = (args) => <Form {...args} />;
-export const Default = Template.bind({});
-Default.args = {};
+export const IndividualForm = Template.bind({});
+IndividualForm.args = {};
+
+const SpouseTemplate = (args) => <SpouseForm {...args} />;
+export const HouseholdForm = SpouseTemplate.bind({});
+HouseholdForm.args = {};
