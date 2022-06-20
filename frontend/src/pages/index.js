8a938e906c588ce66a830a8b56d7413ab3bffd2c
Emily <44536222+emi-hi@users.noreply.github.com>
2022-06-20 16:09:24 -0700

    -removes logout button from index (#184)
    -adds media queries to footer
    -cleans up footer css

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,9 @@
-import { useKeycloak } from '@react-keycloak/web';
 import React from 'react';
 import Layout from '../components/Layout';
 import { Helmet } from 'react-helmet';
 import IndividualLogin from '../components/IndividualLogin';
 
 function Index() {
-  const { keycloak } = useKeycloak();
-
   return (
     <div className="app">
       <Helmet>
@@ -15,16 +12,6 @@ function Index() {
       <Layout>
         <IndividualLogin />
       </Layout>
-      {keycloak.authenticated && (
-        <button
-          type="button"
-          onClick={() => {
-            keycloak.logout();
-          }}
-        >
-          Logout
-        </button>
-      )}
     </div>
   );
 }
