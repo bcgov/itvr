3d30b86b752e5ac30b2c5259133d7a2c41dc9ae3
tim738745 <tim.chen@gov.bc.ca>
2022-06-28 08:40:07 -0700

    itvr-355--logout-button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from '../styles/images/BCID_H_rgb_rev.png';
+import Logout from './Logout';
 
 const Header = (props) => {
   return (
     <div className="page-header">
       <div className="cleanbc-banner">
-        <a
-          href="http://www.gov.bc.ca"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <img src={logo} alt="Government of B.C." />
-        </a>
-        <a
-          href="https://goelectricbc.gov.bc.ca/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          CleanBC Go Electric
-        </a>
+        <div className="left">
+          <a
+            href="http://www.gov.bc.ca"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={logo} alt="Government of B.C." />
+          </a>
+          <a
+            href="https://goelectricbc.gov.bc.ca/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            CleanBC Go Electric
+          </a>
+        </div>
+        <div className="right">
+          <Logout />
+        </div>
       </div>
       <div className="title">
         <h1>Passenger vehicle rebates</h1>
