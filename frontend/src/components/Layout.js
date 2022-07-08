d6cbd8b28a65ecebe6550601d8aa37ff63eb2a7f
tim738745 <tim.chen@gov.bc.ca>
2022-07-08 11:08:20 -0700

    snowplow-analytics

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+  useEffect(() => {
+    if (window.snowplow) {
+      window.snowplow('trackPageView');
+    }
+  }, [location]);
+
   return (
     <div className="layout">
       <Header />
