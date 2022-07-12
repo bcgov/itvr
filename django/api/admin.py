06cd7f49600bc1d69ac1cdb48ee03bdd7da127ae
vibhiquartech <105294710+vibhiquartech@users.noreply.github.com>
2022-07-12 22:39:22 +0530

    cancel initiated applications (#221)
    * cancel initiated applications
    
    * fix sonar bug
    
    * PR suggestions
    
    * remove change_form

--- django/api/admin.py
+++ django/api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models.go_electric_rebate_application import (
     GoElectricRebateApplication,
     SubmittedGoElectricRebateApplication,
+    InitiatedGoElectricRebateApplication,
 )
 from .models.household_member import HouseholdMember
 from .models.go_electric_rebate import GoElectricRebate
@@ -42,7 +43,7 @@ class GoElectricRebateApplicationAdmin(admin.ModelAdmin):
 # by BCeID users.
 @admin.register(SubmittedGoElectricRebateApplication)
 class SubmittedGoElectricRebateApplicationAdmin(admin.ModelAdmin):
-    search_fields = ['drivers_licence', 'id', 'status']
+    search_fields = ["drivers_licence", "id", "status"]
     # disable bulk actions
     actions = None
     exclude = (
@@ -116,3 +117,66 @@ class SubmittedGoElectricRebateApplicationAdmin(admin.ModelAdmin):
 @admin.register(GoElectricRebate)
 class GoElectricRebateAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(InitiatedGoElectricRebateApplication)
+class InitiatedGoElectricRebateApplicationAdmin(admin.ModelAdmin):
+    search_fields = ["drivers_licence", "id", "status"]
+    # disable bulk actions
+    actions = None
+    exclude = (
+        "sin",
+        "doc1",
+        "doc2",
+        "user",
+        "spouse_email",
+        "status",
+        "address",
+        "city",
+        "postal_code",
+        "application_type",
+        "doc1_tag",
+        "doc2_tag",
+        "consent_personal",
+        "consent_tax",
+    )
+    readonly_fields = (
+        "id",
+        "last_name",
+        "first_name",
+        "middle_names",
+        "email",
+        "user_is_bcsc",
+        "drivers_licence",
+        "date_of_birth",
+        "tax_year",
+        "is_legacy",
+    )
+
+    def get_queryset(self, request):
+        return GoElectricRebateApplication.objects.filter(
+            status=GoElectricRebateApplication.Status.HOUSEHOLD_INITIATED
+        )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def response_change(self, request, obj):
+        ret = super().response_change(request, obj)
+        if "cancel_application" in request.POST:
+            obj.status = GoElectricRebateApplication.Status.CANCELLED
+            obj.save(update_fields=["status"])
+        return ret
+
+    def message_user(
+        self,
+        request,
+        message,
+        level=messages.INFO,
+        extra_tags="",
+        fail_silently=False,
+    ):
+        revised_level = level
+        if "cancel_application" in request.POST:
+            revised_level = messages_custom.NEGATIVE_SUCCESS
+        super().message_user(request, message, revised_level, extra_tags, fail_silently)
