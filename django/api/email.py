07d9824c6b6546200dcb3d83395b146bbc15b1fe
tim738745 <tim.chen@gov.bc.ca>
2022-08-30 10:35:10 -0700

    itvr-419--approval-email-changes

--- django/api/email.py
+++ django/api/email.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum
 
 
 def config():
@@ -11,3 +12,30 @@ def config():
         "SENDER_NAME": "CleanBC Go Electric",
         "SEND_EMAIL": os.getenv("SEND_EMAIL", "False") == "True",
     }
+
+
+class FOUR_THOUSAND_REBATE(Enum):
+    ZEV_MAX = 4000
+    ZEV_MID = 2688
+    ZEV_MIN = 1332
+    PHEV_MAX = 2000
+    PHEV_MID = 1334
+    PHEV_MIN = 666
+
+
+class TWO_THOUSAND_REBATE(Enum):
+    ZEV_MAX = 2000
+    ZEV_MID = 1334
+    ZEV_MIN = 666
+    PHEV_MAX = 1000
+    PHEV_MID = 667
+    PHEV_MIN = 333
+
+
+class ONE_THOUSAND_REBATE(Enum):
+    ZEV_MAX = 1000
+    ZEV_MID = 667
+    ZEV_MIN = 333
+    PHEV_MAX = 500
+    PHEV_MID = 334
+    PHEV_MIN = 167
