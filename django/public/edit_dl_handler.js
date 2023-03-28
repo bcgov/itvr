1096d740074e8c4de4ddae75ecd87beb5b211d54
tim738745 <tim.chen@gov.bc.ca>
2023-03-28 11:20:29 -0700

    feat: itvr-469 - allow uppercase letters in DL number entered from console

--- django/public/edit_dl_handler.js
+++ django/public/edit_dl_handler.js
@@ -1,7 +1,7 @@
 const dl = document.getElementById('id_drivers_licence');
 dl.addEventListener('input', (event) => {
     const edit = document.getElementById('edit_drivers_licence');
-    const regex = /^\d+$/;
+    const regex = /^[A-Z0-9]*$/;
     if (dl.value.length < 7 || !regex.test(dl.value)) {
         edit.disabled = true;
     } else {
