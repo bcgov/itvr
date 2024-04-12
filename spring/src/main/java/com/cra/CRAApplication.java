ca0f60c9c97c206bb922e242a491a1ab8f69bfbc
tim738745 <tim.chen@gov.bc.ca>
2024-04-12 14:51:57 -0700

    undo previous security providers change

--- spring/src/main/java/com/cra/CRAApplication.java
+++ spring/src/main/java/com/cra/CRAApplication.java
@@ -2,15 +2,10 @@ package com.cra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.security.Security;
-import com.entrust.toolkit.security.provider.Entrust;
-import iaik.security.provider.IAIK;
 
 @SpringBootApplication
 public class CRAApplication {
   public static void main(String[] args) {
-    Security.addProvider(new Entrust());
-    Security.addProvider(new IAIK());
     SpringApplication.run(CRAApplication.class, args);
   }
 }
