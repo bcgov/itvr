9aaf46cd802a075027002ef670f67b21306185fc
tim738745 <tim.chen@gov.bc.ca>
2024-06-22 22:12:41 -0700

    small change

--- django/api/admin.py
+++ django/api/admin.py
@@ -594,15 +594,10 @@ class ExpiredGoElectricRebateApplicationAdmin(ITVRModelAdmin):
 
 
 @admin.register(LegacyGoElectricRebateApplication)
-class LegacyGoElectricRebateApplicationAdmin(admin.ModelAdmin):
+class LegacyGoElectricRebateApplicationAdmin(ITVRModelAdminStringent):
 
-    actions = None
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
+    def has_delete_permission(self, request, obj=None):
+        return admin.ModelAdmin.has_delete_permission(self, request, obj)
 
     search_fields = ["drivers_licence", "id"]
     exclude = (
